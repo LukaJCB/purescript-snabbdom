42b212e47ca9c13e420b7101fbd94a3169cf95d2
Sebastian Klingler <sklingler93@gmail.com>
2018-09-02 13:59:44 -0500

    Update dependencies

--- test/Main.purs
+++ test/Main.purs
@@ -1,49 +1,43 @@
 module Test.Main where
 
 import Prelude
-import Control.Monad.Aff.AVar (AVAR)
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Class (liftEff)
-import Control.Monad.Eff.Console (CONSOLE)
-import Control.Monad.Eff.Random (RANDOM)
-import Control.Monad.Eff.Unsafe (unsafePerformEff)
-import Control.Monad.Maybe.Trans (lift, runMaybeT)
-import DOM (DOM)
-import DOM.HTML (window)
-import DOM.HTML.Types (htmlDocumentToDocument)
-import DOM.HTML.Window (document)
-import DOM.Node.Document (createElement)
-import DOM.Node.Node (appendChild, textContent)
-import DOM.Node.ParentNode (querySelector, QuerySelector(..))
-import DOM.Node.Types (Element, documentToParentNode, elementToNode)
+import Effect (Effect)
+import Effect.Class (liftEffect)
+import Effect.Unsafe (unsafePerformEffect)
+import Web.HTML (window)
+import Web.HTML.Window (document)
+import Web.HTML.HTMLDocument (toDocument, toParentNode)
+import Web.DOM.Document (createElement)
+import Web.DOM.Node (appendChild, textContent)
+import Web.DOM.ParentNode (querySelector, QuerySelector(..))
+import Web.DOM.Element (Element, toNode)
 import Data.Maybe (Maybe(..), isJust, maybe)
 import Data.Newtype (wrap)
-import Data.StrMap (empty)
-import Snabbdom (VDOM, VNodeProxy, h, patchInitial, text, toVNodeEventObject, toVNodeHookObjectProxy)
+import Data.Map (empty)
+import Control.Monad.Maybe.Trans (lift, runMaybeT)
+import Snabbdom (VNodeProxy, h, patchInitial, text, toVNodeEventObject, toVNodeHookObjectProxy)
 import Test.QuickCheck (Result(..), (===))
 import Test.Unit (suite, test)
 import Test.Unit.Assert (assert)
-import Test.Unit.Console (TESTOUTPUT)
 import Test.Unit.Main (exit, runTest)
 import Test.Unit.QuickCheck (quickCheck)
 
-patchAndGetElement :: forall e. VNodeProxy (dom :: DOM | e) -> Eff (dom:: DOM, vdom :: VDOM | e) (Maybe Element)
+patchAndGetElement :: forall e. VNodeProxy e -> Effect (Maybe Element)
 patchAndGetElement proxy = do
   wndow <- window
   doc <- document wndow
 
-  let htmlDoc = htmlDocumentToDocument doc
-      findInDocument queryStr = querySelector (QuerySelector queryStr) $ documentToParentNode htmlDoc
+  let findInDocument queryStr = querySelector (QuerySelector queryStr) $ toParentNode doc
 
-  node <- createElement "div" htmlDoc
+  node <- createElement "div" $ toDocument doc
   runMaybeT do
     body <- wrap $ findInDocument "body"
-    _ <- lift $ appendChild (elementToNode node) (elementToNode body)
+    _ <- lift $ appendChild (toNode node) (toNode body)
     lift $ patchInitial node proxy
     wrap $ findInDocument "#msg"
 
 
-main :: Eff (console :: CONSOLE, testOutput :: TESTOUTPUT, avar :: AVAR, dom :: DOM, vdom :: VDOM, random :: RANDOM) Unit
+main :: Effect Unit
 main = do
   runTest do
     suite "Snabbdom" do
@@ -51,7 +45,7 @@ main = do
               let message = "Hello World"
                   vNode = createVNode message
                   eff = patchAndGetElement vNode
-              elem <- (liftEff eff)
+              elem <- (liftEffect eff)
               assert "Message should be patched into the DOM" (isJust elem)
               quickCheck (maybe (Failed "failure") (compareTextContent message) elem)
   (exit 0)
@@ -60,8 +54,8 @@ main = do
 
 compareTextContent :: String -> Element -> Result
 compareTextContent message element =
-  let node = elementToNode element
-  in unsafePerformEff (textContent node) === message
+  let node = toNode element
+  in unsafePerformEffect (textContent node) === message
 
 
 
